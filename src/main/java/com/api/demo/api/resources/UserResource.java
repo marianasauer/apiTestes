1eff02f9956ca23822a21d6a8489e7d32e1c8916
Mariana Sauer <marianasauer2004@gmail.com>
2023-06-16 15:01:21 -0300

    class user resource findById

---
+++ src/main/java/com/api/demo/api/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.api.demo.api.resources;
+
+import com.api.demo.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return  ResponseEntity.ok().body(new User(2, "Valdir", "val@gmail.com", "123"));
+    }
+}
