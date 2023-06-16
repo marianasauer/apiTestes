ac613dc828c616684f46a923a68e13f953eef2c6
Mariana Sauer <marianasauer2004@gmail.com>
2023-06-16 16:51:51 -0300

    retornando dto para user resource

--- src/main/java/com/api/demo/api/domain/dto/UserDTO.java
+++ src/main/java/com/api/demo/api/domain/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.api.demo.api.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,8 +15,9 @@ public class UserDTO {
 
     private Integer id;
     private String name;
-
     private String email;
+
+    @JsonIgnore
     private String password;
 
 }
