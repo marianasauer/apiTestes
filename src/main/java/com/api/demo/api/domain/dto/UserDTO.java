74206048ee7235b8487d63d0535707b796ec4cc8
Mariana Sauer <marianasauer2004@gmail.com>
2023-06-16 21:06:19 -0300

    endpoint create para user

--- src/main/java/com/api/demo/api/domain/dto/UserDTO.java
+++ src/main/java/com/api/demo/api/domain/dto/UserDTO.java
@@ -1,6 +1,7 @@
 package com.api.demo.api.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +18,7 @@ public class UserDTO {
     private String name;
     private String email;
 
-    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
 }
--- src/main/java/com/api/demo/api/services/UserService.java
+++ src/main/java/com/api/demo/api/services/UserService.java
@@ -1,6 +1,7 @@
 package com.api.demo.api.services;
 
 import com.api.demo.api.domain.User;
+import com.api.demo.api.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ import java.util.List;
 public interface UserService{
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
