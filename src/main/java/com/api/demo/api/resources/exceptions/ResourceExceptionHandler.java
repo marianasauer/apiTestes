331b22921befae2657a3b4142021ea5a5f14dd38
Mariana Sauer <marianasauer2004@gmail.com>
2023-06-19 14:54:38 -0300

    teste para manipulador de excecoes metodo data integrity

--- src/main/java/com/api/demo/api/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/api/demo/api/resources/exceptions/ResourceExceptionHandler.java
@@ -16,12 +16,13 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError>objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
-        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), request.getRequestURI() );
+        StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(),
+                request.getRequestURI() );
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<StandardError>dataIntegratyViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
+    public ResponseEntity<StandardError>dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request){
         StandardError error = new StandardError(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getMessage(), request.getRequestURI() );
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
